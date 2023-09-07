6612dd6526eb7eef3ef166ffd197075bacd94dfb
Dev-Crusader <yuvrajsingh532004@gmail.com>
2023-09-07 12:28:25 +0530

    Backend intial commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-console.log("BACKEND ... ")
+const express = require('express');
+const  app = express();
+const port = 4000;
+
+app.listen(port, ()=>{
+    console.log(`SERVER RUNNING FINE AT ${port}`)
+})
