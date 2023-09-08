13f51b9c6fb195badbab4790696a7efc1f2d0d39
Dev-Crusader <yuvrajsingh532004@gmail.com>
2023-09-08 11:57:38 +0530

    backend updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const {app} = require('./app')
 const port = process.env.PORT || 4000;
-
+const MONGODB_URI = "mongodb+srv://yuvrajSingh05:moneY341@testcluster.hqa0fzg.mongodb.net/test";
+const {connectToDB} = require('./models/db');
+connectToDB(MONGODB_URI);
 app.listen(port, ()=>{
-    console.log(`SERVER RUNNING FINE AT ${port}`)
+    console.log(`SERVER RUNNING FINE AT ${port}`);
 })
